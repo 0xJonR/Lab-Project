9140f1e0bfca235911d0d30ad1eda56916692467
jawn <teholdkid@gmail.com>
2019-03-03 19:50:21 -0500

    added functionality to projectile and implemented weapon

--- src/backend/Projectile.py
+++ src/backend/Projectile.py
@@ -3,5 +3,23 @@ from .Location import Location
 
 class Projectile(Location):
     #x y
+    #hitscan
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
     def XYCollision(self):
         pass
+
+    def __eq__(self, other):
+        return self.x==other.x && self.y==other.y
+
+    def projectileDistance(self, dT):
+        c = new Projectile(self.x + dT, self.y + dT)
+        return c
+
+    def isCollide(self, projDistance, otherProj):
+        x = projDistance.x
+        y = projDistance.y
+        x1 = otherProj.x
+        y1 = otherProj.y
--- src/backend/Weapon.py
+++ src/backend/Weapon.py
@@ -1,9 +1,19 @@
-class Weapon():
-    def __init__(self, name):
+from .Projectile import Projectile
+from .Location import Location
+
+class Weapon(Location):
+    def __init__(self, name, loc):
         self.name = name
+        self.x = loc.x
+        self.y = loc.y
+
+
     ammo = 30 #30 rounds by
     #weapons should not shoot infintely in a direction
     def shoot(self):
+        #subtract one from ammo
+        self.ammo -= 1
+        new Projectile()
         pass
 
     def reload(self):
