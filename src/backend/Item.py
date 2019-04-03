71a1c982bac67ef5d7aeaab162121eb71ef7bdb8
jawn <24411983+keithdugan@users.noreply.github.com>
2019-04-03 12:02:59 -0400

    fixed weapons classes: made their constructor set their values.

--- src/backend/Item.py
+++ src/backend/Item.py
@@ -26,47 +26,46 @@ class Weapon(Location):
         Projectile(self.x, self.y, self.fireDistance, direction)
         pass
 
-    def reload(self): #TODO IMPLEMENT DELAY AND RELOAD
+    def reload(self): # TODO IMPLEMENT DELAY AND RELOAD
         pass
     def __str__(self):
+
         return self.name
 
-class Pistol(Weapon):
-    name = "pistol"
-    ammo = 10
-    DMG = 25
-    fireRate = 0.5
-    fireDistance = 10
 
+class Pistol(Weapon):
     def __init__(self, loc):
         super().__init__(loc)
+        self.name = "pistol"
+        self.ammo = 10
+        self.DMG = 25
+        self.fireRate = 0.5
+        self.fireDistance = 10
 
 
 class Shotgun(Weapon):
-    ammo = 5
-    DMG = 420
-    fireRate = 0.25
-    fireDistance = 7
     def __init__(self, loc):
         super().__init__(loc)
+        self.ammo = 5
+        self.DMG = 420
+        self.fireRate = 0.25
+        self.fireDistance = 7
 
 
 class Rifle(Weapon):
-    ammo = 30
-    DMG = 300
-    fireRate = 1.0
-    fireDistance = 14
-
     def __init__(self, loc):
         super().__init__(loc)
+        self.ammo = 30
+        self.DMG = 300
+        self.fireRate = 1.0
+        self.fireDistance = 14
 
 
 class Smg(Weapon):
-    ammo = 20
-    DMG = 200
-    fireRate = 1.60
-    fireDistance = 10
 
     def __init__(self, loc):
         super().__init__(loc)
-
+        self.ammo = 20
+        self.DMG = 200
+        self.fireRate = 1.60
+        self.fireDistance = 10
